b1be3b4c4241a2980b68e128342cbabe1894ee7c
Jérémie Henrion <jeremie_henrion@carrefour.com>
2021-09-15 01:03:38 +0200

    aller retour ok

--- WS-CLIENTS/src/soap/clients/Clients.java
+++ WS-CLIENTS/src/soap/clients/Clients.java
@@ -8,10 +8,22 @@ import soap.account.ClientAccount;
 public class Clients {
 	
 	public static void main(String[] args) throws RemoteException {
-		ClientAccount account = new ClientAccount("franc", "demo");
-		account.runClient();
+		String name = "france";
+		String pwd = "demo";
+		ClientAccount account = new ClientAccount(name, pwd);
+		account.register();
+		account.getInfo();
 		
-		ClientBooking booking = new ClientBooking();
-		booking.runClient();
+		ClientBooking booking = new ClientBooking(name, pwd);
+		booking.runClientGet();
+		
+		booking.runClientBooking();
+		
+		account.getInfo();
+		
+		booking.removeTrain(account.idBook);
+		
+		account.getInfo();
+		
 	}
 }
